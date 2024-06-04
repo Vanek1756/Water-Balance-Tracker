a59c65947804464153238835b88fb5ef05ffa73a
babiychuk.vania <ivan.babiichuk@maxbet.ro>
2024-06-04 14:56:30 +0300

    Implemented viewBinding. Created MainActivityViewModel.

--- app/src/main/java/com/babiichuk/waterbalancetracker/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/babiichuk/waterbalancetracker/app/ui/main/MainActivity.kt
@@ -2,14 +2,20 @@ package com.babiichuk.waterbalancetracker.app.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.babiichuk.waterbalancetracker.R
+import com.babiichuk.waterbalancetracker.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
