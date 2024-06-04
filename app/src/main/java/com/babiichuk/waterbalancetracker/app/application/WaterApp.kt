9cd39c75aa67ff9b2e4618cfd70de49e1ce07f7d
babiychuk.vania <ivan.babiichuk@maxbet.ro>
2024-06-04 14:52:21 +0300

    Created Hilt application class.

---
+++ app/src/main/java/com/babiichuk/waterbalancetracker/app/application/WaterApp.kt
@@ -0,0 +1,9 @@
+package com.babiichuk.waterbalancetracker.app.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WaterApp: Application() {
+
+}
